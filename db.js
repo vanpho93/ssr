a2891889ccc3d9ff2c27018a9a66568d437a82fa
Khoa Pham <khoaphp@yahoo.com>
2016-11-22 19:16:14 +0700

    ket thuc

--- db.js
+++ db.js
@@ -16,17 +16,41 @@ function queryDB(sql, cb){
     if(err){
       console.log('LOI KET NOI ' + err);
     }else{
-      client.query(sql, cb);
+      client.query(sql, function(err, result){
+
+      });
     }
   });
 }
 
+
+function query(sql){
+  return new Promise(
+    function(resovle, reject){
+      pool.connect(function(err, client, done){
+        if(err){
+          reject('Loi ket noi');
+        }else{
+          client.query(sql, function(err, result){
+            if(err){
+              reject('Loi query');
+            }else{
+              resovle(result);
+            }
+          });
+        }
+      });
+    }
+  );
+}
 //SELECT * FROM "User" WHERE username = 'xyz' AND password = 'abc'
 
 pool.on('error', function(err, client){
   console.log('LOI:: ' + err);
 });
 
-queryDB(`SELECT * FROM "SanPham"`, function(err, result){
-  console.log(result.rows);
-})
+// queryDB(`SELECT * FROM "SanPham"`, function(err, result){
+//   console.log(result.rows);
+// })
+
+module.exports = query;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var parser = bodyParser.urlencoded({extended: false});
+var query = require('./db.js');
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
@@ -12,5 +15,36 @@ app.get('/', function(req, res){
 });
 
 app.get('/danhsach', function(req, res){
-  res.render('danhsach');
+  query('SELECT * FROM "SanPham"')
+  .then(function(result){
+    res.render('danhsach', {mang: result.rows});
+  })
+  .catch(function(err){
+    res.send(err);
+  })
+});
+
+app.get('/sua/:id', function(req, res){
+  query('SELECT * FROM "SanPham" WHERE id='+req.params.id)
+  .then(function(result){
+    res.render('chitiet',{sanPham: result.rows[0]});
+  })
+  .catch(function(err){
+    res.send(err);
+  })
+});
+
+app.post('/xulyupdate',parser, function(req, res){
+  var id = req.body.id;
+  var tensp = req.body.tensp;
+  var hinh = req.body.hinh;
+  var mota = req.body.mota;
+  query(`UPDATE "SanPham" SET tensp = '${tensp}', hinh='${hinh}',
+        mota='${mota}' WHERE id=${id}`)
+  .then(function(){
+    res.send('Sua thanh cong');
+  })
+  .catch(function(){
+    res.send('Sua that bai');
+  })
 });
